f4699c903c19c288a08d6359932ab436f7bd6871
Andrew Ratnikov <andrewratnikov@Andrews-MacBook-Pro-2.local>
2022-04-03 18:05:12 +0300

    feat: add some styles for aside

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,5 +1,17 @@
+import data from "../../data/keys.json";
+
+import './Aside.css'
+
 const Aside = () => {
-  return <aside>Aside</aside>;
+  return (
+    <aside>
+      <ul className="card">
+        {data.keys.map((key) => (
+          <li>{key.nameEn}</li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
 
 export default Aside;
