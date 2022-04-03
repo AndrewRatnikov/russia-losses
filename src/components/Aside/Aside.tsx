ddee6f79d5d9d08626fafcbe8457b9a7bc5c1928
Andrew Ratnikov <andrewratnikov@Andrews-MacBook-Pro-2.local>
2022-04-03 19:26:36 +0300

    feat: add basic aside

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,13 +1,29 @@
-import data from "../../data/keys.json";
+import { useState } from "react";
 
-import './Aside.css'
+import keyData from "../../data/keys.json";
+import data from '../../data/data.json'
+
+import "./Aside.css";
 
 const Aside = () => {
+  const [active, setActive] = useState("personnel");
+  const activeData: {[key: string]: string | number | null}  = data.data[0]
+
+  const onClickHandler = (id: string) => () => {
+    setActive(id);
+  };
+
   return (
     <aside>
       <ul className="card">
-        {data.keys.map((key) => (
-          <li>{key.nameEn}</li>
+        {keyData.keys.map((key) => (
+          <li
+            key={key.id}
+            className={`card-item ${active === key.id ? "active" : ""}`}
+            onClick={onClickHandler(key.id)}
+          >
+            <span>{key.nameEn}:</span> {activeData[key.id]}
+          </li>
         ))}
       </ul>
     </aside>
