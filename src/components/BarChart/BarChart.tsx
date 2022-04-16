f3adda658d567c1b97168aeb13de6ad87f54bed2
Andrew Ratnikov <ratnikov.am@gmail.com>
2022-04-16 18:15:29 +0300

    feat: add bar chart tooltip

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -13,21 +13,38 @@ const margin = {
 
 const BarChart = ({ data, active }: BarChartProps) => {
   const ref = useD3((svg) => {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
     const { width, height } = svg.node().getBoundingClientRect();
 
-    const x = d3
-      .scaleBand()
-      .domain(data.map((d) => d.date as string))
-      .range([margin.left, width - margin.right])
-      .padding(0.1);
+    const xRange = [margin.left, width - margin.right];
+    const yRange = [height - margin.bottom, margin.top];
 
-    const y = d3
+    const X = d3.map(data, (d) => d.date as string);
+
+    const xDomain = new d3.InternSet(X);
+
+    const xScale = d3.scaleBand(xDomain, xRange).padding(0.1);
+    const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(data, (d) => d[active] as any)])
       .nice()
-      .range([height - margin.bottom, margin.top]);
+      .range(yRange);
 
-    svg.call(zoom);
+    const body = d3.select("body");
+    const tooltip: any = d3.select(".plot-tooltip").node()
+      ? d3.select(".plot-tooltip")
+      : body
+          .append("div")
+          .style("pointer-events", "none")
+          .attr("class", "plot-tooltip");
+
+    svg
+      .call(zoom)
+      .on("pointerenter pointermove", pointermoved)
+      .on("pointerleave", () =>
+        tooltip.style("display", "none").selectChildren().remove()
+      );
 
     if (svg.selectAll("g").nodes().length) {
       svg.selectAll("g");
@@ -41,10 +58,10 @@ const BarChart = ({ data, active }: BarChartProps) => {
       .selectAll("rect")
       .data(data)
       .join("rect")
-      .attr("x", (d) => x(d.date as string) as any)
-      .attr("y", (d) => y(d[active] as number))
-      .attr("height", (d) => y(0) - y(d[active] as number))
-      .attr("width", x.bandwidth());
+      .attr("x", (d) => xScale(d.date as string) as any)
+      .attr("y", (d) => yScale(d[active] as number))
+      .attr("height", (d) => yScale(0) - yScale(d[active] as number))
+      .attr("width", xScale.bandwidth());
 
     function zoom(_svg: any) {
       const extent: [[number, number], [number, number]] = [
@@ -62,16 +79,53 @@ const BarChart = ({ data, active }: BarChartProps) => {
       );
 
       function zoomed(event: any) {
-        x.range(
+        xScale.range(
           [margin.left, width - margin.right].map((d) =>
             event.transform.applyX(d)
           )
         );
         _svg
           .selectAll(".bars rect")
-          .attr("x", (d: any) => x(d.date))
-          .attr("width", x.bandwidth());
+          .attr("x", (d: any) => xScale(d.date))
+          .attr("width", xScale.bandwidth());
       }
+    }
+
+    function pointermoved(event: any) {
+      const [xCoordinate] = d3.pointer(event); // [x, y]
+      const scale = d3.scaleQuantize(xDomain).domain(xRange);
+      const xValue = scale(xCoordinate);
+
+      const tooltipValue = data.find((d) => d.date === xValue)?.[active];
+
+      tooltip
+        .style("display", "block")
+        .attr("class", "plot-tooltip")
+        .style("padding", "2px")
+        .style("position", "fixed")
+        .style("background-color", "#fff")
+        .style("border", "1px solid #000")
+        .style("z-index", "9999")
+        .html(() => {
+          return `<div>${xValue}<br /><b>${tooltipValue}</b></div>`;
+        });
+
+      const { width: tooltipWidth, height: tooltipHeight } = tooltip
+        .node()
+        .getBoundingClientRect();
+
+      let y = event.y + 15;
+      let x = event.x + 15;
+
+      if (x + tooltipWidth > windowWidth) {
+        x = x - tooltipWidth - 15;
+      }
+
+      if (y + tooltipHeight > windowHeight) {
+        y = y - tooltipHeight - 15;
+      }
+
+      tooltip.style("top", `${y}px`).style("left", `${x}px`);
     }
   });
 
