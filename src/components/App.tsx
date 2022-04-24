a4694ec71e0f3eed483305686008a73eb16e3d76
Andrew Ratnikov <ratnikov.am@gmail.com>
2022-04-24 17:30:39 +0300

    refactor: create header component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,12 @@
 import Container from "./Container";
+import Header from "./Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <header className="header">
-        <h1>Combat losses of the enemy</h1>
-      </header>
-
+      <Header />
       <Container />
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Combat losses of the enemy</h1>
+    </header>
+  );
+};
+
+export default Header;
