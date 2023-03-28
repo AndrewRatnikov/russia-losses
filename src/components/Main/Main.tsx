1cad4fbac38360e878f637cf343539a350d02771
Andrew Ratnikov <ratnikov.am@gmail.com>
2023-03-28 22:03:10 +0300

    feature: add day/week/month view

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,17 +4,47 @@ import data from "../../data/data.json";
 import keys from "../../data/keys.json";
 import { ActiveItem } from "../Container/Container";
 import { addDays, transformData } from "./helpers";
+import "./Main.css";
+import { useState } from "react";
+
+type Legend = "day" | "week" | "month";
 
 const Main = ({ activeItem }: { activeItem: ActiveItem }) => {
+  const [legend, setLegend] = useState<Legend>("day");
+
   const _keys = keys.keys.map((key) => key.id);
   const transformedData: any = addDays(
     transformData(data.data, _keys).reverse() as any,
-    "day"
+    legend
   );
 
+  const legendHandler = (predefinedLegend: Legend) => () => {
+    setLegend(predefinedLegend);
+  };
+
   return (
-    <main>
+    <main className="main">
       <BarChart data={transformedData} active={activeItem} />
+      <div className="legend">
+        <span
+          className={`legend-item ${legend === "day" ? "active" : ""}`}
+          onClick={legendHandler("day")}
+        >
+          days
+        </span>
+        <span
+          className={`legend-item ${legend === "week" ? "active" : ""}`}
+          onClick={legendHandler("week")}
+        >
+          weeks
+        </span>
+        <span
+          className={`legend-item ${legend === "month" ? "active" : ""}`}
+          onClick={legendHandler("month")}
+        >
+          months
+        </span>
+      </div>
     </main>
   );
 };
